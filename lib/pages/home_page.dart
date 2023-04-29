3545b333b11bd79129b8d7506ea66fcf125b19ba
Sara <104867595+SaraSpg@users.noreply.github.com>
2023-04-29 11:14:22 +0200

    home 2

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -16,33 +16,40 @@ class HomePage extends StatelessWidget {
               color: Colors.red,
             ),
           ),
-          Container(
-            color: Colors.white54,
-            child: Padding(
-              padding: const EdgeInsets.only(top: 20, left: 30, right: 30),
-              child: Row(
-                children: const [
-                  Icon(
-                    Icons.description_outlined,
-                    color: Colors.blue,
-                  ),
-                  Tasks(title: 'Quizzettone', points: 15),
-                ],
+          Padding(
+            padding: const EdgeInsets.only(top: 20, left: 30, right: 30),
+            child: Container(
+              color: Colors.grey[100],
+              child: Padding(
+                padding: const EdgeInsets.all(5),
+                child: Row(
+                  children: const [
+                    Icon(
+                      Icons.description_outlined,
+                      color: Colors.blue,
+                    ),
+                    Tasks(title: 'Quizzettone', points: 15),
+                  ],
+                ),
               ),
             ),
           ),
-          Container(
-            color: Colors.white54,
-            child: Padding(
-              padding: const EdgeInsets.only(top: 20, left: 20, right: 20),
-              child: Row(
-                children: const [
-                  Icon(
-                    Icons.face,
-                    color: Colors.blue,
-                  ),
-                  Tasks(title: 'Daily Footprint', points: 5),
-                ],
+
+          Padding(
+            padding: const EdgeInsets.only(top: 20, left: 30, right: 30),
+            child: Container(
+              color: Colors.grey[100],
+              child: Padding(
+                padding: const EdgeInsets.all(5),
+                child: Row(
+                  children: const [
+                    Icon(
+                      Icons.face,
+                      color: Colors.blue,
+                    ),
+                    Tasks(title: 'Daily Footprint', points: 15),
+                  ],
+                ),
               ),
             ),
           ),
