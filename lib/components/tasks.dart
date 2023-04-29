b1c430ee467a0a0b11cf2579ec2830bd129498f0
Sara <104867595+SaraSpg@users.noreply.github.com>
2023-04-29 11:05:11 +0200

    home 1

---
+++ lib/components/tasks.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class Tasks extends StatelessWidget {
+  const Tasks(
+      {super.key,
+      required this.title,
+      required this.points,
+      this.onPressed}); //da mettere il tempo
+  
+  final String title;
+  
+  final int points;
+  final void Function()? onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.only(),
+        child: GestureDetector(
+          onTap: onPressed,
+          child: Container(
+            color: Colors.transparent,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                
+                Padding(
+                    padding: const EdgeInsets.only(left: 24),
+                    child: Text(
+                      title,
+                      style: const TextStyle(
+                          color: Colors.black,
+                          fontSize: 20,
+                          fontWeight: FontWeight.w500),
+                    )),
+                const Spacer(),
+                Padding(
+                  padding: const EdgeInsets.only(left: 10),
+                  child: Text(
+                    points.toString(),
+                    style: const TextStyle(
+                        color: Colors.black,
+                        fontSize: 16,
+                        fontWeight: FontWeight.w300),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:eco_app/components/tasks.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -5,17 +6,48 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Padding(
-          padding: const EdgeInsets.all(32),
-          child: Container(
-            height: 150,
-            color: Colors.red,
+    return Scaffold(
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(32),
+            child: Container(
+              height: 150,
+              color: Colors.red,
+            ),
           ),
-        ),
-        //Sara inizia qui
-      ],
+          Container(
+            color: Colors.white54,
+            child: Padding(
+              padding: const EdgeInsets.only(top: 20, left: 30, right: 30),
+              child: Row(
+                children: const [
+                  Icon(
+                    Icons.description_outlined,
+                    color: Colors.blue,
+                  ),
+                  Tasks(title: 'Quizzettone', points: 15),
+                ],
+              ),
+            ),
+          ),
+          Container(
+            color: Colors.white54,
+            child: Padding(
+              padding: const EdgeInsets.only(top: 20, left: 20, right: 20),
+              child: Row(
+                children: const [
+                  Icon(
+                    Icons.face,
+                    color: Colors.blue,
+                  ),
+                  Tasks(title: 'Daily Footprint', points: 5),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
